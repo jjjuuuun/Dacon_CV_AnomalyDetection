ae82acca169bde8ebbbcd549fc7113bb9b653703
jjjuuuun <jun2250599@gmail.com>
2022-04-18 22:41:03 +0900

    Related Image Show

---
+++ image.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+from trainDF import ColumnCollection
+
+class SamplingImage():
+    """"""
+    def __init__(self, path):
+        """"""
+        self.path = path
+
+    def clsImshow(self,):
+        """"""
